a5e75026ffef72c7640b3ca94da8d132a159caf3
rjeff-dev <jeff_xd00@hotmail.com>
2025-10-13 16:50:39 -0300

    ultimas

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const watch = require('gulp-watch');
 
 function styles(){
-    return gulp.src('src/styles/style.scss')
+    return gulp.src('src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/styles'));
 }
 
 exports.default = gulp.parallel(styles);
+exports.watch = function(){
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
+}
