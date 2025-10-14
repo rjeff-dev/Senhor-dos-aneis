6bc05bbcf840b7ddd8ef62d67fa412eacff9833b
rjeff-dev <jeff_xd00@hotmail.com>
2025-10-14 16:09:35 -0300

    02

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const watch = require('gulp-watch');
+const cleanCss = require('gulp-clean-css')
+const imagemin = require("gulp-imagemin")
+// const watch = require('gulp-watch');
 
 function styles(){
-    return gulp.src('src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/styles'));
+    return gulp.src('src/styles/style.scss') // Garante que só o arquivo principal seja compilado
+        .pipe(sass().on('error', sass.logError)) 
+        .pipe(cleanCss()) 
+        // CORREÇÃO FINAL: Salva diretamente em 'dist'
+        .pipe(gulp.dest('./dist')); 
 }
 
-exports.default = gulp.parallel(styles);
+function images(){
+    // NOVO PADRÃO: './src/images/**/*.{jpg,png,gif,svg}'
+    // Isso garante que você pegue QUALQUER arquivo com essas extensões em todas as subpastas.
+    // É o padrão mais seguro para evitar pastas vazias.
+
+    return gulp.src('./src/images/**/*.{jpg,jpeg,png,gif,svg}', { encoding: false }) 
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function(){
     gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
 }
